a60446075c050b17fc6aaf46db5f3ad6425a12a8
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-02-03 10:03:57 +0100

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Maryna Bolotska on 03/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
