c385440cd57e7370a70e38288e2cd7a8b73845de
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-02-04 20:39:00 +0100

    Food Category Horizontal List

---
+++ Yummie/CustomViews/CollectionViewCells/CategoryCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Maryna Bolotska on 04/02/24.
+//
+
+import Foundation
---
+++ Yummie/Models/DishCategory.swift
@@ -0,0 +1,8 @@
+//
+//  DishCategory.swift
+//  Yummie
+//
+//  Created by Maryna Bolotska on 04/02/24.
+//
+
+import Foundation
