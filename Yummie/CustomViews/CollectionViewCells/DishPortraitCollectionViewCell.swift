0b30a84dda6833c094125dd549c4f2622461aa74
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-02-05 20:51:28 +0100

    Chef's Special Horizontal List

---
+++ Yummie/CustomViews/CollectionViewCells/DishPortraitCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  DishPortraitCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Maryna Bolotska on 05/02/24.
+//
+
+import Foundation
---
+++ Yummie/Models/Dish.swift
@@ -0,0 +1,8 @@
+//
+//  Dish.swift
+//  Yummie
+//
+//  Created by Maryna Bolotska on 05/02/24.
+//
+
+import Foundation
